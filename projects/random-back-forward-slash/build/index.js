ce08a36dc404e4fa6ae9b5e49f4c9817454be432
Andre Patacas <guerreiroapatacas@gmail.com>
2021-01-31 21:10:59 +0000

    maze like generator

---
+++ projects/random-back-forward-slash/build/index.js
@@ -0,0 +1,27 @@
+/// <reference path="../../p5.d/p5.global-mode.d.ts" />
+let slash_size;
+function setup() {
+    createCanvas(windowWidth * .99, windowHeight * .98);
+    background(0);
+    slash_size = random(30, 120);
+    strokeWeight(random(1, 3));
+    stroke(255, 255, 255);
+}
+let w = 0;
+let h = 0;
+function draw() {
+    stroke(random(100, 255), random(100, 255), random(100, 255));
+    if (Math.random() > random(.1, .9))
+        line(w, h, w + slash_size, h + slash_size);
+    else
+        line(w, h + slash_size, w + slash_size, h);
+    w += slash_size;
+    if (w > width) {
+        h += slash_size;
+        w = 0;
+        if (h > height) {
+            noLoop();
+        }
+    }
+}
+//# sourceMappingURL=index.js.map
