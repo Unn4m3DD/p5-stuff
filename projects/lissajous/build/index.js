3995fa57a8e3c617edc8bd17000b6a3c1ef9b90e
Andre Patacas <guerreiroapatacas@gmail.com>
2021-02-01 01:05:16 +0000

    lissajous curves guideline added

--- projects/lissajous/build/index.js
+++ projects/lissajous/build/index.js
@@ -6,31 +6,82 @@ function setup() {
     background(0);
     rectMode(CENTER);
     ellipseMode(CENTER);
+    for (let i = 0; i < width / box_size - 1; i++) {
+        points.push([]);
+        for (let j = 0; j < height / box_size - 1; j++) {
+            points[i].push([]);
+        }
+    }
 }
 let t = 0;
-let time_step = 0.01;
+let time_step = 0.03;
+let points = [];
 function draw() {
+    background(0);
     for (let i = 1; i < width / box_size - 1; i++) {
         stroke(255, 255, 255);
         ellipse(i * box_size + box_size / 2, box_size / 2, circle_radius);
         stroke(255, 0, 0);
-        //line(i * box_size + box_size / 2 + circle_radius / 2 * cos(t * i), 0, i * box_size + box_size / 2 + circle_radius / 2 * cos(t * i), height)
+        strokeWeight(1);
+        line(i * box_size + box_size / 2 + circle_radius / 2 * cos(t * i), 0, i * box_size + box_size / 2 + circle_radius / 2 * cos(t * i), height);
+        strokeWeight(10);
+        point(i * box_size + box_size / 2 + circle_radius / 2 * cos(t * i), box_size / 2 + circle_radius / 2 * sin(t * i));
     }
     for (let i = 1; i < height / box_size - 1; i++) {
         stroke(255, 255, 255);
         ellipse(box_size / 2, i * box_size + box_size / 2, circle_radius);
         stroke(255, 0, 0);
-        //line(0, i * box_size + box_size / 2 + circle_radius / 2 * sin(i * t), width, i * box_size + box_size / 2 + circle_radius / 2 * sin(i * t))
+        strokeWeight(1);
+        line(0, i * box_size + box_size / 2 + circle_radius / 2 * sin(i * t), width, i * box_size + box_size / 2 + circle_radius / 2 * sin(i * t));
+        strokeWeight(10);
+        point(box_size / 2 + circle_radius / 2 * cos(t * i), i * box_size + box_size / 2 + circle_radius / 2 * sin(t * i));
     }
     for (let i = 1; i < width / box_size - 1; i++) {
         for (let j = 1; j < height / box_size - 1; j++) {
-            stroke(255, 0, 0);
-            strokeWeight(2);
-            line(i * box_size + box_size / 2 + cos(t * i) * circle_radius / 2, j * box_size + box_size / 2 + sin(t * j) * circle_radius / 2, i * box_size + box_size / 2 + cos((t + time_step) * i) * circle_radius / 2, j * box_size + box_size / 2 + sin((t + time_step) * j) * circle_radius / 2);
+            points[i][j].push({
+                x: i * box_size + box_size / 2 + cos(t * i) * circle_radius / 2,
+                y: j * box_size + box_size / 2 + sin(t * j) * circle_radius / 2
+            });
+        }
+    }
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    for (let i = 0; i < points.length; i++) {
+        for (let j = 0; j < points[i].length; j++) {
+            for (let k = 1; k < points[i][j].length; k++) {
+                line(points[i][j][k - 1].x, points[i][j][k - 1].y, points[i][j][k].x, points[i][j][k].y);
+            }
         }
     }
     t += time_step;
-    if (t > Math.PI * 2)
+    if (t > Math.PI * 2) {
         noLoop();
+        for (let i = 1; i < width / box_size - 1; i++) {
+            stroke(255, 255, 255);
+            ellipse(i * box_size + box_size / 2, box_size / 2, circle_radius);
+        }
+        for (let i = 1; i < height / box_size - 1; i++) {
+            stroke(255, 255, 255);
+            ellipse(box_size / 2, i * box_size + box_size / 2, circle_radius);
+        }
+        background(0);
+        for (let i = 1; i < width / box_size - 1; i++) {
+            for (let j = 1; j < height / box_size - 1; j++) {
+                points[i][j].push({
+                    x: i * box_size + box_size / 2 + cos(t * i) * circle_radius / 2,
+                    y: j * box_size + box_size / 2 + sin(t * j) * circle_radius / 2
+                });
+            }
+        }
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        for (let i = 0; i < points.length; i++) {
+            for (let j = 0; j < points[i].length; j++) {
+                for (let k = 1; k < points[i][j].length; k++) {
+                    line(points[i][j][k - 1].x, points[i][j][k - 1].y, points[i][j][k].x, points[i][j][k].y);
+                }
+            }
+        }
+    }
 }
 //# sourceMappingURL=index.js.map
